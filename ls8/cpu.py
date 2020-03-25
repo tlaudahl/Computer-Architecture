39142864a7a0963e97cf86ddb1ced462bcb761f0
tlaudahl <travis.laudahl@gmail.com>
2020-03-25 11:50:48 -0500

    Day 2 MVP Done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,12 +1,11 @@
 """CPU functionality."""
 
-# '_'
-
 import sys
 
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -30,27 +29,47 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     '0b10000010', # LDI R0,8
+        #     '0b00000000',
+        #     '0b00001000',
+        #     '0b01000111', # PRN R0
+        #     '0b00000000',
+        #     '0b00000001', # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
+
+        try:
+            path = sys.argv[1]
+            with open(path) as f:
+                for line in f:
+                    line = line.split('#')[0]
+                    line = line.strip()
+
+                    if line == '':
+                        continue
+                    
+                    value = int(line, 2)
+
+                    self.ram[address] = value
+                    address += 1
+        except IndexError:
+            print('No file given')
+        except:
+            print('Error')
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -80,13 +99,21 @@ class CPU:
 
         while running:
             IR = self.ram_read(self.pc)
-            # self.trace()
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
 
-            run_code = IR[self.pc]
-            print(run_code)
-
-            if run_code == 00:
-                self.pc += 1
 
             if IR == HLT:
-                self.running = False
+                self.pc = 0
+                running = False
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == MUL:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc += 3
+            else:
+                print('Unknown Instruction')
