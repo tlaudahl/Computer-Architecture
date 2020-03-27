09db994b123739896d03f8f3f1808e48868c1c6a
tlaudahl <travis.laudahl@gmail.com>
2020-03-27 12:31:42 -0500

    MVP Done for Sprint challenge

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,13 +2,22 @@
 
 import sys
 
-HLT = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
-MUL = 0b10100010
-POP = 0b01000110
+HLT  = 0b00000001
+LDI  = 0b10000010
+PRN  = 0b01000111
+MUL  = 0b10100010
+ADD  = 0b10100000
+POP  = 0b01000110
 PUSH = 0b01000101
-SP = 7
+CALL = 0b01010000
+RET  = 0b00010001
+CMP  = 0b10100111
+JMP  = 0b01010100
+JEQ  = 0b01010101
+JNE  = 0b01010110
+L    = 0 # Less-than: during a `CMP`, set to 1 if registerA is less than registerB, zero otherwise.
+G    = 0 # Greater-than: during a `CMP`, set to 1 if registerA is greater than registerB, zero otherwise.
+E    = 0 # Equal: during a `CMP`, set to 1 if registerA is equal to registerB, zero otherwise.
 
 
 class CPU:
@@ -19,7 +28,9 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
-        self.reg[SP] = 0xf4 # 244
+        self.sp = 7
+        self.FL = 0b00000000 # 0
+        self.reg[self.sp] = 0xf4 # 244
 
     def ram_read(self, MAR):
         if MAR > 255:
@@ -103,17 +114,79 @@ class CPU:
             elif IR == MUL:
                 self.alu('MUL', operand_a, operand_b)
                 self.pc += 3
+            elif IR == ADD:
+                self.alu('ADD', operand_a, operand_b)
+                self.pc += 3
             elif IR == POP:
-                val = self.ram[self.reg[SP]]
+                val = self.ram[self.reg[self.sp]]
                 reg = self.ram_read(self.pc + 1)
                 self.reg[reg] = val
-                self.reg[SP] += 1
+                self.reg[self.sp] += 1
                 self.pc += 2
             elif IR == PUSH:
                 reg = self.ram_read(self.pc + 1)
                 val = self.reg[reg]
-                self.reg[SP] -= 1
-                self.ram[self.reg[SP]] = val
+                self.reg[self.sp] -= 1
+                self.ram[self.reg[self.sp]] = val
                 self.pc += 2
+            elif IR == CALL:
+                val = self.pc+2
+                reg = self.ram[self.pc+1]
+                subroutine = self.reg[reg]
+                self.reg[self.sp] -= 1
+                self.ram[self.reg[self.sp]] = val
+                self.pc = subroutine
+            elif IR == RET:
+                address = self.reg[self.sp]
+                self.pc = self.ram[address]
+                self.reg[self.sp] += 1
+            elif IR == CMP:
+                self.FL = CMP
+                if self.reg[operand_a] < self.reg[operand_b]:
+                    L = 1
+                    G = 0
+                    E = 0
+                    self.FL = CMP - 0b00000100
+                elif self.reg[operand_a] > self.reg[operand_b]:
+                    L = 0
+                    G = 1
+                    E = 0
+                    self.FL = CMP - 0b00000010
+                elif self.reg[operand_a] == self.reg[operand_b]:
+                    L = 0
+                    G = 0
+                    E = 1
+                    self.FL = CMP - 0b00000001
+                self.pc += 3
+            elif IR == JMP:
+                # Jump to the address stored in the given register.
+                # Set the `PC` to the address stored in the given register.
+
+                # Machine code:
+                # 01010100 00000rrr
+                # 54 0r
+                self.pc = self.reg[operand_a]
+            elif IR == JEQ:
+                # If `equal` flag is set (true), jump to the address stored in the given register.
+
+                # Machine code:
+                # 01010101 00000rrr
+                # 55 0r
+                if E == 1:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+            elif IR == JNE:
+                # If `E` flag is clear (false, 0), jump to the address stored in the given register.
+
+                # Machine code:
+                # ```
+                # 01010110 00000rrr
+                # 56 0r
+                # ```
+                if E == 0:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
             else:
-                print('Unknown Instruction')
+                print('Unknown Instruction', IR)
